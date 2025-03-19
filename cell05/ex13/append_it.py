820b8ff55b29cc46be1f1eef7015071379144442
Sasitorn Jaroenpompong <sjaroenp@z2t16c3.42bangkok.com>
2025-03-19 17:00:34 +0700

    add file

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+import sys
+import re
+
+# i=1
+if len(sys.argv) > 1 :
+  for i in range(1, len(sys.argv)):
+    parameter = sys.argv[i]
+    if len(re.findall('ism',parameter)) > 0:
+        True
+    else:
+        print(f"{parameter}ism")
+else:
+   print("none")
